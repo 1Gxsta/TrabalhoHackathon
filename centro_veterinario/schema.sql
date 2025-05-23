8f2c0773467816099204a4eceaf5ca01189fd6d1
biibiahh24 <bii.biahh24@gmail.com>
2025-05-23 17:34:14 -0300

    Update schema.sql

--- centro_veterinario/schema.sql
+++ centro_veterinario/schema.sql
@@ -1,29 +1,34 @@
--- Criação das tabelas
+-- Tabela de usuários
 CREATE TABLE IF NOT EXISTS Usuarios (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    email TEXT UNIQUE NOT NULL,
-    senha TEXT NOT NULL
+    nome VARCHAR(45) NOT NULL,
+    email VARCHAR(45) NOT NULL UNIQUE,
+    senha VARCHAR(45) NOT NULL
 );
 
+-- Tabela de animais
 CREATE TABLE IF NOT EXISTS Animais (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    nome TEXT NOT NULL,
-    idade INTEGER NOT NULL,
-    peso REAL NOT NULL,
-    raca TEXT,
+    nome VARCHAR(45) NOT NULL,
+    idade INTEGER CHECK(idade >= 0),
+    peso REAL CHECK(peso > 0),
+    raca VARCHAR(45),
     id_usuario INTEGER NOT NULL,
-    FOREIGN KEY(id_usuario) REFERENCES Usuarios(id) ON DELETE CASCADE
+    FOREIGN KEY (id_usuario) REFERENCES Usuarios(id) ON DELETE CASCADE
 );
 
+-- Tabela de consultas
 CREATE TABLE IF NOT EXISTS Consultas (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     id_animal INTEGER NOT NULL,
+    
     vomito BOOLEAN DEFAULT 0,
     febre BOOLEAN DEFAULT 0,
     perda_apetite BOOLEAN DEFAULT 0,
     diarreia BOOLEAN DEFAULT 0,
-    descricao TEXT,
+
+    descricao VARCHAR(150),
     data_consulta TEXT NOT NULL,
-    FOREIGN KEY(id_animal) REFERENCES Animais(id) ON DELETE CASCADE
+
+    FOREIGN KEY (id_animal) REFERENCES Animais(id) ON DELETE CASCADE
 );
